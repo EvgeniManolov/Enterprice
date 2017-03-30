52a3cad07e2e64ccfa0ce497e2f34a7041cb7f58
lorishte <lora.milcheva@outlook.com>
2017-03-30 19:00:37 +0300

    Routes changes

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,41 @@ const User = require('mongoose').model('User');
 const encryption = require('./../utilities/encryption');
 
 module.exports = {
+
+    registerPost:(req, res) => {
+        let registerArgs = req.body;
+
+        User.findOne({email: registerArgs.email}).then(user => {
+            let errorMsg = '';
+            if (user) {
+                errorMsg = 'User with the same username exists!';
+            } else if (registerArgs.password !== registerArgs.repeatedPassword) {
+                errorMsg = 'Passwords do not match!'
+            }
+
+            if (errorMsg) {
+                registerArgs.error = errorMsg;
+                res.render('user/register', registerArgs)
+            } else {
+                let salt = encryption.generateSalt();
+                let passwordHash = encryption.hashPassword(registerArgs.password, salt);
+
+                let userObject = {
+                    email: registerArgs.email,
+                    passwordHash: passwordHash,
+                    fullName: registerArgs.fullName,
+                    salt: salt
+                };
+
+                User.create(userObject).then(user => {
+
+                        res.redirect('/')
+
+                })
+            }
+        })
+    },
+
     loginGet: (req, res) => {
         res.render('home/index');
     },
