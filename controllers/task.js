337a6da0ed14435dbee7f940d05a80ff2923d17b
MJordanov81 <marian.jordanov@gmail.com>
2017-04-02 00:59:13 +0300

    task creation reworked

--- controllers/task.js
+++ controllers/task.js
@@ -11,25 +11,15 @@ module.exports = {
 
     taskCreatePost: (req, res) => {
 
-        console.log(req.body);
 
-        for (var key in req.body) {
-            if (req.body.hasOwnProperty(key)) {
-                let item = req.body[key];
-                console.log(item);
-            }
-        }
+        let taskArgs = req.body;
 
-        let taskArgsArray = req.body;
+        console.log(taskArgs);
 
-        taskArgsArray.forEach(function(taskArgs)
-        {
-            console.log(req.body);
-
-            Team.findOne({teamName: taskArgs.taskTeam}).then(team => {
+            Team.findOne({teamName: taskArgs.taskTeamId}).then(team => {
                 taskArgs.taskTeamId = team.id;
 
-                Project.findOne({projectName: taskArgs.taskProject}).then(project => {
+                Project.findOne({projectName: taskArgs.taskProjectId}).then(project => {
 
                     taskArgs.taskProjectId = project._id;
 
@@ -42,8 +32,18 @@ module.exports = {
                             }
 
                             else {
-                                project.projectTeamName = team.teamName;
-                                res.render('./task/create', {project: project})
+
+                                team.tasks.push(task.id);
+                                team.save(err => {
+                                    if (err) {
+                                        res.redirect('./task/create', {error: err.message});
+                                    }
+
+                                    else {
+                                        project.projectTeamName = team.teamName;
+                                        res.render('./task/create', {project: project})
+                                    }
+                                })
                             }
                         })
 
@@ -51,7 +51,6 @@ module.exports = {
                 })
 
             })
-        })
 
     }
 };
